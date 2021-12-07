89f12950f3194b6f6e37d6c74364173d14556c5f
Thomas Bjeldbak Madsen <thomaseshes@gmail.com>
2021-12-07 19:04:42 +0100

    Got scanning and storing names in Spiller[i].navn to work, but can't get the score tracking to work

--- Dartscore/Dartscore.c
+++ Dartscore/Dartscore.c
@@ -1,74 +1,154 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include<string.h>
 
-void getName (char name[]);
+void getName (char navn[]);
+
+struct Spiller {
+    char navn[50];
+    int  score;
+    int  kaste_point;
+};
 
 int main(void){
     
-    int start_score = 500, antal_spillere, point;
+    int start_score = 500, antal_spillere, kaste_point, i = 0, k = 0;
+    char navne[50];
+    struct Spiller Spiller[i];
 
-    printf("Velkommen til Thomas' Dart score program!\n Indtast hvor mange spillere i er: ");
-    scanf("%d", antal_spillere);
+    printf("\nVelkommen til Thomas' Dart score program!\nIndtast hvor mange spillere i er: ");
+    scanf("%d", &antal_spillere);
 
-    printf("\n\nFantastisk, i er %d spillere, indtast nu navn på hver spiller: ");
-    getName(fullName[]);
+    printf("\n\nFantastisk, i er %d spillere, indtast nu navn på hver spiller: ", antal_spillere);
+
+    // for (i = 0; i != antal_spillere; i++)
+    // {
+    //     // fgets(Spiller[i].navn, 50, stdin);
+    //     // getName(navne);
+    //     // strcpy(Spiller[i].navn, navne);
+
+    //     scanf("%s", &Spiller[i].navn);
+    // }
 
     do
     {
-        getName(fullName[])
-    } while (fullName[] != antal_spillere);
+        scanf("%s", &Spiller[i].navn);
+        i++;
+    } while (i != antal_spillere);
     
-    printf("\nHej %s, herlige navne. I kan nu begynde at spille!\n\n", fullName)
+    printf("\n");
 
-    for (i = 0; i < fullName[i]; i++)
+    for (i = 0; i < antal_spillere; i++)
     {
-        printf("%s tur.\n\n", fullName[i]);
+        printf("Spiller %d's navn er %s\n", i+1, Spiller[i].navn);
+    }
+    
+    printf("\nHerlige navne. I kan nu begynde at spille!\n");
 
-            printf("Indtast point for 1. kast: ");
-            scanf("%d", score_for_første_kast_til_den_pågældende_spiller);
+    printf("--------------------------------------------------------------------\n");
 
-            point_til_den_pågældende_spiller = point_til_den_pågældende_spiller - score_for_første_kast_til_den_pågældende_spiller
+    i = 0;
 
-            printf("\nIndtast point for 2. kast: ");
-            scanf("%d", score_for_andet_kast_til_den_pågældende_spiller);
+    // while (i =! antal_spillere)
+    // {
+    //     Spiller[i].score = 500;
+    //     i++;
+    // }
 
-            point_til_den_pågældende_spiller = point_til_den_pågældende_spiller - score_for_første_kast_til_den_pågældende_spiller
+    Spiller[0].score = 500;
+    Spiller[1].score = 500;
 
-            printf("\nIndtast point for 3. kast: ");
-            scanf("%d", score_for_tredje_kast_til_den_pågældende_spiller);
+    printf("%s har %d point", Spiller[1].navn, Spiller[1].score);
+    
+    // do
+    // {
+    //     Spiller[i].score = 500;
+    //     i++;
+    // } while (i != antal_spillere);
 
-            point_til_den_pågældende_spiller = point_til_den_pågældende_spiller - score_for_første_kast_til_den_pågældende_spiller
+    // for (i = 0; i =! antal_spillere; i++)
+    // {
 
-            printf("Scoren er indtil videre er:\n")
+    //     Spiller[i].kaste_point = 0;
 
-            for (i = 0; i < fullName[i]; i++)
-            {
-                printf("%s har %d point.\n", fullName[i], /* point variable */)
-            }
+    //     printf("%s tur.\n\n", Spiller[i].navn);
+
+    //         printf("Indtast point for 1. kast: ");
+    //         scanf("%d", &Spiller[i].kaste_point);
+
+    //         printf("\nIndtast point for 2. kast: ");
+    //         scanf("%d", &Spiller[i].kaste_point);
+
+    //         printf("\nIndtast point for 3. kast: ");
+    //         scanf("%d", &Spiller[i].kaste_point);
+            
+    //         Spiller[i].score = Spiller[i].score - Spiller[i].kaste_point;
+
+    //         printf("Scoren er indtil videre er:\n");
+
+    //         for (k = 0; k < antal_spillere; k++)
+    //         {
+    //             printf("%s har %d point.\n", Spiller[k].navn, Spiller[k].score);
+    //         }
             
                
-    };
+    // };
+
+    do
+    {
+        Spiller[i].kaste_point = 0;
+
+        printf("%s tur.\n\n", Spiller[i].navn);
+
+            printf("Indtast point for 1. kast: ");
+            scanf("%d", &Spiller[i].kaste_point);
+
+            printf("\nIndtast point for 2. kast: ");
+            scanf("%d", &Spiller[i].kaste_point);
+
+            printf("\nIndtast point for 3. kast: ");
+            scanf("%d", &Spiller[i].kaste_point);
+            
+            Spiller[i].score = Spiller[i].score - Spiller[i].kaste_point;
+
+            printf("Scoren er indtil videre er:\n");
+
+            do
+            {
+                printf("%s har %d point.\n", Spiller[k].navn, Spiller[k].score);
+                k++;
+            } while (k != antal_spillere);
+
+            // for (k = 0; k < antal_spillere; k++)
+            // {
+            //     printf("%s har %d point.\n", Spiller[k].navn, Spiller[k].score);
+            // }
+        i++;
+    } while (Spiller[i].score > 0);
+    
+    
     
     /* Implementer at hvis man lander på 1 eller går i minus skal man tilbage til den score man startede på inden runden startede. */
     /* Print final scoreboard */
 
 }
 
-void getName (char name[]) {
+
+// void getName (char navne[]) {
     
-    char *fullName = (char*) malloc(50);
+//     char *fullName = (char*) malloc(50);
 
-    /*Checks if memory allocation was sucessfull*/
-    if (fullName == NULL) {
-        printf("Memory allocation failed");
-        exit(0);
-    }
+//     /*Checks if memory allocation was sucessfull*/
+//     if (fullName == NULL) {
+//         printf("Memory allocation failed");
+//         exit(0);
+//     }
 
-    //Is able to obtain a string including spaces
-    gets(fullName);
+//     //Is able to obtain a string including spaces
+//     gets(fullName);
     
-    strcpy(name, fullName);
+//     strcpy(navne, fullName);
 
-    free(fullName);
-}
+//     free(fullName);
+// };
